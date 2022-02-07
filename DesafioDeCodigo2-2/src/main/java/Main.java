6c1e612b4457eda09b988ece299792f2c7dfda8b
TiagoViniciusF <tiago_vinicius_ferreira@hotmail.com>
2022-02-07 01:16:58 -0300

    Reupando Bootcamp

---
+++ DesafioDeCodigo2-2/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------");
+        for (int i = 0; i <= 4; i += 1){
+            System.out.println("|                                     |");
+        }
+        System.out.print("---------------------------------------");
+    }
+}
